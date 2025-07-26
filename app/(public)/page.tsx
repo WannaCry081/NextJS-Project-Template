ba1985b349d8a8a4304c01f79ace156bfcab7b7b
Ly <liraedata59@gmail.com>
2025-07-26 13:29:37 +0800

    feat: ✨ add initial page component with welcome message

---
+++ app/(public)/page.tsx
@@ -0,0 +1,10 @@
+export default function Page() {
+  return (
+    <main className="w-full h-dvh">
+      <div className="flex flex-col items-center justify-center h-full">
+        <h1 className="text-4xl font-bold">Welcome to Next.js!</h1>
+        <span>Developed by WannaCry081</span>
+      </div>
+    </main>
+  );
+}
