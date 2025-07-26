daf4f701651e4f608c13e932fb9e685c5680768c
Ly <liraedata59@gmail.com>
2025-07-26 17:08:40 +0800

    chore: 🔨 add getEnvVar function to retrieve environment variables

---
+++ constants/env.ts
@@ -0,0 +1,10 @@
+function getEnvVar(k: keyof NodeJS.ProcessEnv): string {
+  const v = process.env[k];
+  if (!v) {
+    throw new Error(`Missing environment variable: ${k}`);
+  }
+  return v;
+}
+
+// Environment variables
+export const API_URL = getEnvVar("NEXT_PUBLIC_API_URL");
