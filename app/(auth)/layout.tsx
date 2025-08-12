e57119b7839a3341fc5a6870322f55851c05b10a
shielamae02 <shiela.mlepon@gmail.com>
2025-08-12 23:27:27 +0800

    chore: 🔧 refactor Layout component structure for improved semantics and styling

--- app/(auth)/layout.tsx
+++ app/(auth)/layout.tsx
@@ -2,10 +2,16 @@ import React, { PropsWithChildren } from "react";
 
 export default function Layout({ children }: PropsWithChildren) {
   return (
-    <React.Fragment>
-      <header></header>
-      <main>{children}</main>
-      <footer></footer>
-    </React.Fragment>
+    <div className="min-h-screen flex flex-col justify-between bg-background">
+      {/* Main Content */}
+      <section className="flex flex-1 items-center justify-center w-full">
+        <div className="w-full max-w-md px-4">{children}</div>
+      </section>
+
+      {/* Footer */}
+      <footer className="w-full py-4 px-6 text-center text-xs text-muted-foreground">
+        © {new Date().getFullYear()} WannaCry081. All rights reserved.
+      </footer>
+    </div>
   );
 }
