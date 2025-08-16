147320b89935a785e2bffa5cd037f68462e5bb11
shielamae02 <shiela.mlepon@gmail.com>
2025-08-17 02:57:02 +0800

    chore: 💬  move Logo component in authentication folder

--- components/auth/components/Logo.tsx
+++ components/auth/components/Logo.tsx
@@ -8,8 +8,8 @@ const Logo: React.FC<LogoProps> = ({ variant = "default" }) => {
   return (
     <Link href="/">
       <div className="flex items-center space-x-2">
-        <div className="size-6 sm:size-8 rounded-sm sm:rounded-lg flex items-center justify-center bg-foreground">
-          <div className="size-2 sm:size-4 rounded-full bg-background"></div>
+        <div className="size-8 rounded-lg flex items-center justify-center bg-foreground">
+          <div className="size-4 rounded-full bg-background"></div>
         </div>
         {variant === "default" && (
           <span className="font-bold text-sm sm:text-lg">WannaCry081</span>
