89036c7665df5c411ab14c588ea6af6daf235591
shielamae02 <shiela.mlepon@gmail.com>
2025-08-17 00:13:42 +0800

    feat: ✨ add Logo component with customizable variant prop

---
+++ components/shared/Logo.tsx
@@ -0,0 +1,18 @@
+interface LogoProps {
+  variant?: "default" | "icon";
+}
+
+const Logo: React.FC<LogoProps> = ({ variant = "default" }) => {
+  return (
+    <div className="flex items-center space-x-2">
+      <div className="size-6 sm:size-8 rounded-sm sm:rounded-lg flex items-center justify-center bg-foreground">
+        <div className="size-2 sm:size-4 rounded-full bg-background"></div>
+      </div>
+      {variant === "default" && (
+        <span className="font-bold text-sm sm:text-lg">WannaCry081</span>
+      )}
+    </div>
+  );
+};
+
+export default Logo;
