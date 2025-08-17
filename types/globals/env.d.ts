9e1dde9b7d1211ca525bb64216a830f1ee0b4fc8
shielamae02 <shiela.mlepon@gmail.com>
2025-08-17 21:32:25 +0800

    chore: 💬 fix typo in NEXTAUTH_SECRET variable name in env.d.ts

--- types/globals/env.d.ts
+++ types/globals/env.d.ts
@@ -2,7 +2,7 @@ declare namespace NodeJS {
   interface ProcessEnv {
     NEXT_PUBLIC_BASE_URL: string;
     NEXT_PUBLIC_APP_URL: string;
-    NEXTAUTH_SECRETS: string;
+    NEXTAUTH_SECRET: string;
     NEXTAUTH_MAX_AGE: string;
     GITHUB_CLIENT_ID: string;
     GITHUB_CLIENT_SECRET: string;
